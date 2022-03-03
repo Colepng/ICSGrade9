24d35d794248176f82818c685b3ae69dccde33d4
Colepng <85180852+Colepng@users.noreply.github.com>
2022-03-03 09:35:37 -0500

    Add files via upload

---
+++ Python/Python/Basic/1.07/1.py
@@ -0,0 +1,3 @@
+user_name = input("Please input your name")
+user_age = input("Pease input your age ")
+print("Hello", user_name, "you are", user_age, "years old. Thank you for inputting your name and age")
