4c0f5fdbc5fc27cce9155b181229930d99f44eb3
Colepng <gamingwithcole7@gmail.com>
2022-06-02 09:38:41 -0400

    Create CYU 6.py

---
+++ Python/Python/Test/CYU 6.py
@@ -0,0 +1,28 @@
+#QS1
+# #counter
+# num = 7
+# while num < 703:
+#     print(num)
+#     num += 2
+
+# Q2
+# #Variables 
+# print("The loop will stop when your enter 1")
+# pos_count = 0
+# neg_count = 0
+# zero_count = 0
+# while True:
+#     num = int(input("Please input a number "))
+#     if num == 1:    #checking if they entered a 1
+#         break
+#     #if then number entered is not 1 then it check what case it could fall under
+#     elif num > 0: 
+#         pos_count += 1
+
+#     elif num < 0: 
+#         neg_count += 1
+
+#     else:
+#         zero_count += 1
+# #tells the user how many of each they entered
+# print(f"You entered {pos_count} positive numbers, {neg_count} negative numbers and {zero_count} zeros")
